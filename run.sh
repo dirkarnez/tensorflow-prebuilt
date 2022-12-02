38420e3c9fd7bb55844708bdc6bf88042060de6b
Dirk Arnez <smalldirkalex@gmail.com>
2022-12-02 23:12:54 +0800

    - update run.sh

--- run.sh
+++ run.sh
@@ -10,7 +10,8 @@ export PATH="/C/Windows/System32:$PATH"
 export JAVA_HOME="/C/Users/Administrator/Downloads/bazel-v5.3.2-msys2-mingw64-x86_64-posix-seh-rev0-8.1.0/openjdk-11.0.1_windows-x64_bin/jdk-11.0.1/bin"
 # export BAZEL_SH=%ROOT%\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\bin\bash
 
-# git checkout -f v2.9.1
+# git checkout -f v2.6.0
+# bazel version before 3.9
 export MSYS_NO_PATHCONV=1
 export MSYS2_ARG_CONV_EXCL="*"
 
